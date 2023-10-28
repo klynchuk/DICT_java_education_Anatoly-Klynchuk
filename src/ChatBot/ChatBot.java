305bd53d77d7e3dd9d361e06bfd3993bec59806c
klynchuk <a.k.klynchuk@student.khai.edu>
2023-10-29 02:46:54 +0300

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,6 @@
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is BOT.");
+        System.out.println("I was created in 2004.");
+    }
+}
