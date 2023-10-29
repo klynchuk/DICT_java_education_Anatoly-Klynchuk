1ddd880504c69cc0dc26d97930778632428c6a58
klynchuk <a.k.klynchuk@student.khai.edu>
2023-10-29 03:27:42 +0300

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,6 +1,16 @@
+import java.util.Scanner; // Импортируем класс Scanner
+
 public class ChatBot {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is BOT.");
-        System.out.println("I was created in 2004.");
+        System.out.println("Hello! My name is Bot.");
+        int birthYear = java.time.Year.now().getValue();
+        System.out.println("I was created in " + birthYear + ".");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, remind me your name.");
+        String userName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + userName + "!");
     }
 }
