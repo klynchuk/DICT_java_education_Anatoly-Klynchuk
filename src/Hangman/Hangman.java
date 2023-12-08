779ee6eb9e62b13ab03cd90cd340b4103d95a3f7
klynchuk <a.k.klynchuk@student.khai.edu>
2023-12-08 03:06:00 +0200

    Hangman {2-nd} stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,2 +1,22 @@
-package Hangman;public class Hangman {
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
+        System.out.println("Guess the word: >");
+
+        String wordToGuess = "java"; // Слово, яке гравцеві потрібно вгадати
+        String playerGuess = scanner.nextLine();
+
+        if (playerGuess.equals(wordToGuess)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+
+        scanner.close();
+    }
 }
