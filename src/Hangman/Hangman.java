704bae1c740a956dfabf8c40e9a5a3b1ce690041
klynchuk <a.k.klynchuk@student.khai.edu>
2023-12-08 03:34:07 +0200

    Hangman {7-th} stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Hangman {
     public static void main(String[] args) {
@@ -8,79 +10,76 @@ public class Hangman {
 
         System.out.println("HANGMAN");
 
-        // Список слів для вибору
-        String[] words = {"python", "java", "javascript", "kotlin"};
+        while (true) {
+            System.out.println("Type \"play\" to play the game, \"exit\" to quit:");
+            String command = scanner.nextLine();
 
-        // Випадковим чином вибираємо слово зі списку
+            if (command.equals("exit")) {
+                break;
+            } else if (command.equals("play")) {
+                playHangman(scanner, random);
+            } else {
+                System.out.println("Unknown command. Type \"play\" to play the game, \"exit\" to quit.");
+            }
+        }
+
+        System.out.println("Goodbye!");
+    }
+
+    private static void playHangman(Scanner scanner, Random random) {
+        String[] words = {"python", "java", "javascript", "kotlin"};
         int randomIndex = random.nextInt(words.length);
         String wordToGuess = words[randomIndex];
-
-        int attemptsLeft = 8; // Кількість залишених спроб
-
-        // Створюємо рядок з дефісами, який представляє відкриті та закриті літери
         int lives = 8; // Кількість життів
+        Set<Character> guessedLetters = new HashSet<>();
         StringBuilder wordInProgress = new StringBuilder();
         for (int i = 0; i < wordToGuess.length(); i++) {
             wordInProgress.append("-");
         }
+        while (lives > 0) {
+            System.out.println(wordInProgress);
+            System.out.println("Input a letter: >");
+            String inputLetter = scanner.nextLine().toLowerCase();
 
-        while (attemptsLeft > 0) {
-            while (lives > 0) {
-                System.out.println(wordInProgress);
-                System.out.println("Input a letter: >");
-                String inputLetter = scanner.nextLine();
-
-                if (wordToGuess.contains(inputLetter)) {
-                    // Якщо буква зустрічається в слові
-                    for (int i = 0; i < wordToGuess.length(); i++) {
-                        if (wordToGuess.charAt(i) == inputLetter.charAt(0)) {
-                            wordInProgress.setCharAt(i, inputLetter.charAt(0));
-
-                            // Буква зустрічається в слові
-                            if (wordInProgress.toString().contains(inputLetter)) {
-                                // Гравець вже намагався вгадати цю букву
-                                System.out.println("No improvements");
-                                lives--;
-                            } else {
-                                // Відкрити букву у слові
-                                for (int j = 0; j < wordToGuess.length(); j++) {
-                                    if (wordToGuess.charAt(j) == inputLetter.charAt(0)) {
-                                        wordInProgress.setCharAt(j, inputLetter.charAt(0));
-                                    }
+            if (inputLetter.length() != 1 || !Character.isLowerCase(inputLetter.charAt(0))) {
+                System.out.println("Please enter a lowercase English letter");
+            } else {
+                char letter = inputLetter.charAt(0);
+                if (guessedLetters.contains(letter)) {
+                    System.out.println("You've already guessed this letter");
+                } else {
+                    guessedLetters.add(letter);
+                    if (wordToGuess.contains(inputLetter)) {
+                        if (wordInProgress.toString().contains(inputLetter)) {
+                            System.out.println("No improvements");
+                            lives--;
+                        } else {
+                            for (int i = 0; i < wordToGuess.length(); i++) {
+                                if (wordToGuess.charAt(i) == letter) {
+                                    wordInProgress.setCharAt(i, letter);
                                 }
                             }
                         }
+                    } else {
+                        System.out.println("That letter doesn't appear in the word");
+                        lives--;
                     }
-                } else {
-                    // Якщо буква не зустрічається в слові
-                    // Буква не зустрічається в слові
-                    System.out.println("That letter doesn't appear in the word");
-                    attemptsLeft--;
-                    lives--;
-                }
 
-                if (wordInProgress.toString().equals(wordToGuess)) {
                     if (!wordInProgress.toString().contains("-")) {
-                        // Гравець вгадав всі літери
                         System.out.println(wordInProgress);
                         System.out.println("You guessed the word!");
+                        System.out.println("You guessed the word " + wordToGuess + "!");
                         System.out.println("You survived!");
                         break;
-                    }
-                }
-
-                if (!wordInProgress.toString().equals(wordToGuess)) {
-                    // Гравець не вгадав слово
-                    System.out.println("Thanks for playing!");
-                    System.out.println("We'll see how well you did in the next stage");
-                    if (lives == 0) {
-                        // Гравець витратив всі життя
-                        System.out.println("You lost!");
                     }
                 }
             }
         }
 
-        scanner.close();
+        if (lives == 0) {
+            System.out.println("You lost!");
+        }
+
+        guessedLetters.clear();  // Очищуємо введені букви для наступної гри
     }
 }
