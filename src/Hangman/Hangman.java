e3088eed269e51c78ad3b75dd1a520155bbb284a
klynchuk <a.k.klynchuk@student.khai.edu>
2023-12-08 03:23:07 +0200

    Hangman {6-th} stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -15,57 +15,70 @@ public class Hangman {
         int randomIndex = random.nextInt(words.length);
         String wordToGuess = words[randomIndex];
 
-        // Виводимо підказку з першими двома літерами
-        StringBuilder hint = new StringBuilder();
-        hint.append(wordToGuess.charAt(0));
-        hint.append(wordToGuess.charAt(1));
-        for (int i = 2; i < wordToGuess.length(); i++) {
-            hint.append("-");
-        }
+        int attemptsLeft = 8; // Кількість залишених спроб
 
         // Створюємо рядок з дефісами, який представляє відкриті та закриті літери
+        int lives = 8; // Кількість життів
         StringBuilder wordInProgress = new StringBuilder();
         for (int i = 0; i < wordToGuess.length(); i++) {
             wordInProgress.append("-");
         }
 
-        System.out.println("Guess the word " + hint + ": >");
-
-        int attemptsLeft = 8; // Кількість залишених спроб
-
         while (attemptsLeft > 0) {
-            System.out.println("Input a letter: >");
-            String inputLetter = scanner.nextLine();
+            while (lives > 0) {
+                System.out.println(wordInProgress);
+                System.out.println("Input a letter: >");
+                String inputLetter = scanner.nextLine();
 
-            if (inputLetter.length() == 1 && Character.isLetter(inputLetter.charAt(0))) {
                 if (wordToGuess.contains(inputLetter)) {
                     // Якщо буква зустрічається в слові
                     for (int i = 0; i < wordToGuess.length(); i++) {
                         if (wordToGuess.charAt(i) == inputLetter.charAt(0)) {
                             wordInProgress.setCharAt(i, inputLetter.charAt(0));
+
+                            // Буква зустрічається в слові
+                            if (wordInProgress.toString().contains(inputLetter)) {
+                                // Гравець вже намагався вгадати цю букву
+                                System.out.println("No improvements");
+                                lives--;
+                            } else {
+                                // Відкрити букву у слові
+                                for (int j = 0; j < wordToGuess.length(); j++) {
+                                    if (wordToGuess.charAt(j) == inputLetter.charAt(0)) {
+                                        wordInProgress.setCharAt(j, inputLetter.charAt(0));
+                                    }
+                                }
+                            }
                         }
                     }
                 } else {
                     // Якщо буква не зустрічається в слові
+                    // Буква не зустрічається в слові
                     System.out.println("That letter doesn't appear in the word");
                     attemptsLeft--;
+                    lives--;
                 }
 
                 if (wordInProgress.toString().equals(wordToGuess)) {
-                    // Гравець вгадав всі літери
-                    System.out.println(wordInProgress);
-                    System.out.println("You survived!");
-                    break;
+                    if (!wordInProgress.toString().contains("-")) {
+                        // Гравець вгадав всі літери
+                        System.out.println(wordInProgress);
+                        System.out.println("You guessed the word!");
+                        System.out.println("You survived!");
+                        break;
+                    }
                 }
-            } else {
-                System.out.println("Please enter a single letter");
-            }
-        }
 
-        if (!wordInProgress.toString().equals(wordToGuess)) {
-            // Гравець не вгадав слово
-            System.out.println("Thanks for playing!");
-            System.out.println("We'll see how well you did in the next stage");
+                if (!wordInProgress.toString().equals(wordToGuess)) {
+                    // Гравець не вгадав слово
+                    System.out.println("Thanks for playing!");
+                    System.out.println("We'll see how well you did in the next stage");
+                    if (lives == 0) {
+                        // Гравець витратив всі життя
+                        System.out.println("You lost!");
+                    }
+                }
+            }
         }
 
         scanner.close();
