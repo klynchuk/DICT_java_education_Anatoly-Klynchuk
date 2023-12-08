3e75d0d37e9a43151c281ddc8a1285eeadf75ca4
klynchuk <a.k.klynchuk@student.khai.edu>
2023-12-08 03:11:09 +0200

    Hangman {4-th} stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,21 +1,27 @@
 import java.util.Scanner;
 import java.util.Random;
-
 public class Hangman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-
         System.out.println("HANGMAN");
-
         // Список слів для вибору
         String[] words = {"python", "java", "javascript", "kotlin"};
-
         // Випадковим чином вибираємо слово зі списку
         int randomIndex = random.nextInt(words.length);
         String wordToGuess = words[randomIndex];
 
         System.out.println("Guess the word: >");
+        // Виводимо підказку з першими двома літерами
+        StringBuilder hint = new StringBuilder();
+        hint.append(wordToGuess.charAt(0));
+        hint.append(wordToGuess.charAt(1));
+
+        for (int i = 2; i < wordToGuess.length(); i++) {
+            hint.append("-");
+        }
+
+        System.out.println("Guess the word " + hint + ": >");
 
         String playerGuess = scanner.nextLine();
 
